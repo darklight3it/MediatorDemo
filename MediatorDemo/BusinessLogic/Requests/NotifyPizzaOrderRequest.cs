f8d70d12b30eaf3784752027db934c1314375016
Darklight3it <darklight3it@gmail.com>
2016-12-11 14:39:01 +0100

    NotifyPizzaOrderRequest

---
+++ MediatorDemo/BusinessLogic/Requests/NotifyPizzaOrderRequest.cs
@@ -0,0 +1,9 @@
+namespace MediatorDemo.BusinessLogic.Requests
+{
+    using System;
+
+    public class NotifyPizzaOrderRequest
+    {
+        public Guid OrderId { get; set; }
+    }
+}
--- MediatorDemo/Controllers/DemoController.cs
+++ MediatorDemo/Controllers/DemoController.cs
@@ -17,7 +17,57 @@
             this.mediator = mediator;
         }
 
+        #region Request Pizza
+
         public async void RequestPizza()
+        {
+            var request = new PizzaOrderRequest
+                {
+                    Address = "Via di prova 2, Crespellano",
+                    Order = new Dictionary<PizzaType, int> { { PizzaType.Patatine, 15 }, { PizzaType.Margherita, 1 } }
+                };
+
+            PizzaOrderResponse response = await mediator.SendAsync(request);
+
+            Console.Out.Write(
+                "The bill is " + response.Bill + " EUR, and it will be delivered to " + request.Address + " at "
+                + response.DeliveryTime + "\n");
+
+            Console.ReadKey();
+        }
+
+        public void NotifyPizzaOrder()
+        {
+            var request = new NotifyPizzaOrderRequest();
+        }
+
+        #endregion
+
+        #region Cancel Pizza Order
+
+        public void CancelPizzaOrder()
+        {
+            var request = new PizzaOrderCancellationRequest { OrderId = Guid.NewGuid() };
+
+            try
+            {
+                mediator.Send(request);
+
+                Console.Out.Write("The order " + request.OrderId + " is cancelled\n");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.Out.Write("The order cannot be cancelled\n");
+            }
+
+            Console.ReadKey();
+        }
+
+        #endregion
+
+        #region Request Pizza Advanced
+
+        public async void RequestPizzaAdvanced()
         {
             var request = new PizzaOrderRequest
                 {
@@ -43,22 +93,6 @@
             Console.ReadKey();
         }
 
-        public void CancelPizzaOrder()
-        {
-            var request = new PizzaOrderCancellationRequest { OrderId = Guid.NewGuid() };
-
-            try
-            {
-                mediator.Send(request);
-
-                Console.Out.Write("The order " + request.OrderId + " is cancelled\n");
-            }
-            catch (ArgumentNullException)
-            {
-                Console.Out.Write("The order cannot be cancelled\n");
-            }
-
-            Console.ReadKey();
-        }
+        #endregion
     }
 }
